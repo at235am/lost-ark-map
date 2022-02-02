d2b4363a8694785245df32b7899d3bab086f684f
at235am <at235am@gmail.com>
2022-02-02 11:54:26 -0800

    initial setup + theme support

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import { HelmetProvider } from "react-helmet-async";
+import { ThemeStateProvider } from "./contexts/ThemeContext";
+import { UIStateProvider } from "./contexts/UIContext";
+
+import { BrowserRouter } from "react-router-dom";
+// import { AuthProvider } from "./contexts/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeStateProvider>
+      <UIStateProvider>
+        {/* <AuthProvider> */}
+        <HelmetProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </HelmetProvider>
+        {/* </AuthProvider> */}
+      </UIStateProvider>
+    </ThemeStateProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
